870cfb60c59f8eb09b5d70f0dc23a55a056d5eb3
promiseedet <edetpromise019@gmail.com>
2025-06-09 12:04:51 +0100

    delete message fixed

--- src/app/pages/layout/ChatBox.jsx
+++ src/app/pages/layout/ChatBox.jsx
@@ -4,19 +4,19 @@ import profileImage from "../../../assets/images/profile image.jpg";
 import Button_Component from "../../../components/ButtonsComponent";
 import send from "../../../assets/icons/Send it.svg";
 import back from "../../../assets/icons/Arrow back black.svg";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-auth.js";
 import {
   getFirestore,
-  setDoc,
-  doc,
   addDoc,
+  doc,
   collection,
   query,
-  where,
-  getDocs,
+  orderBy,
+  onSnapshot,
+  deleteDoc,
   serverTimestamp,
 } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
 import { AuthContext } from "../../Context/AuthContext";
@@ -43,6 +43,13 @@ export default function ChatBox({ selectedUser, mobileChatBox, onBack }) {
   const [Message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
   const { isLoggedIn } = useContext(AuthContext);
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [chat]);
 
   const generateChatId =
     isLoggedIn.localeCompare(selectedUser.id) > 0
@@ -52,6 +59,7 @@ export default function ChatBox({ selectedUser, mobileChatBox, onBack }) {
   async function sendMessage(e) {
     if (Message.trim() === "") return;
     console.log(generateChatId);
+    setIsloading(true);
     setMessage("");
     try {
       const MessageRef = collection(db, "chats", generateChatId, "messages");
@@ -60,29 +68,47 @@ export default function ChatBox({ selectedUser, mobileChatBox, onBack }) {
         timestamp: serverTimestamp(),
         from: isLoggedIn,
       });
+      setIsloading(false);
     } catch (error) {
       setMessage("");
+      setIsloading(false);
       toast.error("unable to send message");
       console.log(error);
     }
   }
 
   useEffect(() => {
-    async function getChatMessages(user1, user2) {
-      const chatID = generateChatID(user1, user2);
-      const messagesRef = collection(db, "chats", chatID, "messages");
+    const messagesRef = collection(db, "chats", generateChatId, "messages");
 
-      const snapshot = await getDocs(messagesRef);
-      const message = snapshot.docs.map((doc) => doc.data());
-      setChat(message);
-    }
+    const q = query(messagesRef, orderBy("timestamp"));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const messages = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setChat(messages);
+    });
+
+    return () => unsubscribe();
   });
+
+  async function deleteMessage(messageId) {
+    console.log(messageId);
+    try {
+      await deleteDoc(doc(db, "chats", generateChatId, "messages", messageId));
+      toast.success("Message deleted");
+    } catch (err) {
+      console.error(err);
+      toast.error("Failed to delete message");
+    }
+  }
 
   return (
     <div
       className={`${
         !mobileChatBox && "hidden"
-      } fixed top-0 left-0 h-screen bg-white w-screen p-1 md:px-15 lg:static lg:w-[70%] lg:p-3 lg:float-right lg:border-l lg:h-full`}
+      } overflow-y-clip fixed top-0 left-0 h-screen bg-gray-100 w-screen p-1 md:px-15 lg:static lg:w-[70%] lg:p-3 lg:float-right lg:border-l lg:h-full`}
     >
       <ToastContainer />
 
@@ -92,7 +118,7 @@ export default function ChatBox({ selectedUser, mobileChatBox, onBack }) {
         </h1>
       ) : (
         <>
-          <header className='w-full p-2 space-x-4'>
+          <header className='w-full p-2 space-x-4 sticky bg-white border-b'>
             <img
               src={back}
               className='w-6 float-left mt-3 lg:hidden'
@@ -107,9 +133,35 @@ export default function ChatBox({ selectedUser, mobileChatBox, onBack }) {
             <h1 className='inline-block'>{selectedUser.username}</h1>
           </header>
 
-          {JSON.stringify(chat)}
+          <div className='space-y-3 flex flex-col items-start overflow-y-scroll'>
+            {chat.map((msg) => (
+              <div
+                ref={scrollRef}
+                key={msg.id}
+                className={`flex ${
+                  msg.from === isLoggedIn ? "justify-end" : "justify-start"
+                } w-full`}
+              >
+                <div
+                  className={`message p-4 rounded-lg w-fit max-w-[80%] ${
+                    msg.from === isLoggedIn
+                      ? "bg-green-500 text-white"
+                      : "bg-white text-black"
+                  }`}
+                >
+                  <p>{msg.text}</p>
+                  <button
+                    onClick={() => deleteMessage(msg.id)}
+                    className='text-xs text-red-500'
+                  >
+                    delete
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
 
-          <footer className='fixed bottom-0 flex space-x-3 bg-gray-300 p-2 w-screen -ml-2 md:w-[87%] lg:-ml-3 lg:w-[56%] xl:w-[46%]'>
+          <footer className='fixed bottom-0 flex space-x-3 bg-gray-300 pl-2.5 py-2 w-screen -ml-2 md:w-[87%] lg:-ml-3 lg:w-[56%] xl:w-[46%]'>
             <textarea
               value={Message}
               onChange={(e) => setMessage(e.target.value)}
