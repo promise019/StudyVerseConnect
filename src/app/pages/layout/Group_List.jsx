5c31b48320b7ba5f0a33750a8f617ac92c35846f
promiseedet <edetpromise019@gmail.com>
2025-06-10 22:40:35 +0100

    portfolio ready features, Group will be done later

--- src/app/pages/layout/Group_List.jsx
+++ src/app/pages/layout/Group_List.jsx
@@ -1,3 +1,11 @@
 export default function Group_Lists() {
-    return 'groups'
+  return (
+    <div className='text-center'>
+      <h1 className='font-bold'>
+        Group chats are available for premium users and free only to certain
+        regoins
+      </h1>
+      <p className='italic'>But for you, coming Soon...</p>
+    </div>
+  );
 }
